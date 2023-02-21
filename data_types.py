8e272c27c0e570cfa3f9926a3c259c17e04be607
Cynthia <kiturcynthia0@gmail.com>
2023-02-21 15:23:21 +0300

    my first commit

---
+++ data_types.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter any number"))
+num2 = int(input("Enter a second number"))
+
+print(num1)
+print(num2
+      )
---
+++ variable.py
@@ -0,0 +1,5 @@
+name = "Kingston"
+age = 22
+country = "Bahams"
+
+ print(f"My name is {name} and I am {age}  years of age")
